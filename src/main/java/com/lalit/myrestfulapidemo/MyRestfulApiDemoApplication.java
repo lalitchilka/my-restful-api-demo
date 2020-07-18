f969290648f58d56fa49dc08d7300609c3d218fb
lalitchilka <lalitchilka@gmail.com>
2020-07-18 13:43:59 +0530

    Initial commit for empty maven project

---
+++ src/main/java/com/lalit/myrestfulapidemo/MyRestfulApiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.lalit.myrestfulapidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyRestfulApiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyRestfulApiDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lalit/myrestfulapidemo/MyRestfulApiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lalit.myrestfulapidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyRestfulApiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
