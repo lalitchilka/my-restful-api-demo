aca515d38c6a3b9ba59e302d4add0b7bf9d8dc41
lalitchilka <lalitchilka@gmail.com>
2020-07-18 14:01:24 +0530

    Added User controller. Closes #6

---
+++ src/main/java/com/lalit/myrestfulapidemo/controllers/UserController.java
@@ -0,0 +1,13 @@
+package com.lalit.myrestfulapidemo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UserController {
+
+    @RequestMapping({"", "/", "index", "index.html"})
+    public String getUsers(){
+        return "index";
+    }
+}
